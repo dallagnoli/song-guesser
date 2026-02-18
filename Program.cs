e6b58ebae73915c6964637425c27cf43bf3606f2
Gabriel Dallagnoli <187823668+dallagnoli@users.noreply.github.com>
2026-02-18 16:37:49 -0300

    basic random word reading

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+string filePath = "list.txt";
+
+if (File.Exists(filePath))
+{
+    string[] words = File.ReadAllLines(filePath);
+
+    if (words.Length == 0)
+    {
+        Console.WriteLine("Error: The list.txt file is empty. Please add some words to it!");
+        return;
+    }
+
+    Random rng = new Random();
+    int index = rng.Next(words.Length);
+    string secretWord = words[index];
+    Console.WriteLine($"The secret word is: {secretWord}");
+}
+else
+{
+    Console.WriteLine("Error: list.txt not found!");
+}
